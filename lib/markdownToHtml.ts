101e5da03d2680805e261fadee272184f9705409
Philip Etter <uniqueuptopermutation@proton.me>
2024-04-06 15:14:07 -0700

    Added mdx and mathjax

--- lib/markdownToHtml.ts
+++ lib/markdownToHtml.ts
@@ -1,7 +1,22 @@
 import { remark } from "remark";
 import html from "remark-html";
+import remarkMdx from 'remark-mdx'
+import rehypeMathjax from 'rehype-mathjax'
+import rehypeStringify from 'rehype-stringify'
+import remarkMath from 'remark-math'
+import remarkParse from 'remark-parse'
+import remarkRehype from 'remark-rehype'
+import {unified} from 'unified'
 
 export default async function markdownToHtml(markdown: string) {
-  const result = await remark().use(html).process(markdown);
+  const result = await unified()
+  .use(html)
+  .use(remarkParse)
+  .use(remarkMdx)
+  .use(remarkMath)
+  .use(remarkRehype)
+  .use(rehypeMathjax)
+  .use(rehypeStringify)
+  .process(markdown); 
   return result.toString();
 }
