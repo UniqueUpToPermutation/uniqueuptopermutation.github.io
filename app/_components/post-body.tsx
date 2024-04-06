2ce1961acab4b2257c339b5cad1a40a9a3ca7b69
Philip Etter <uniqueuptopermutation@proton.me>
2024-04-06 16:49:57 -0700

    Centered math output in blog

--- app/_components/post-body.tsx
+++ app/_components/post-body.tsx
@@ -6,11 +6,13 @@ type Props = {
 
 export function PostBody({ content }: Props) {
   return (
-    <div className="max-w-2xl mx-auto">
-      <div
-        className={markdownStyles["markdown"]}
-        dangerouslySetInnerHTML={{ __html: content }}
-      />
+    <div className={markdownStyles["mjx-container"]}>
+      <div className={"max-w-2xl mx-auto"}>
+        <div
+          className={markdownStyles["markdown"]}
+          dangerouslySetInnerHTML={{ __html: content }}
+        />
+      </div>
     </div>
   );
 }
